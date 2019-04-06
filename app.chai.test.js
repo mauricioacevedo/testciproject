a3f8db6d7fb21b0ae0b15f4da757569bda80db72
Mauricio Acevedo <mauricio.acevedo@gmail.com>
2019-04-06 10:29:16 -0500

    change test library2

--- app.chai.test.js
+++ app.chai.test.js
@@ -3,7 +3,8 @@ const sum = require('./app');
 
 describe('Mocha test sum', ()=>{
     it('It should sum 1 + 2 = 3',()=>{
-        console.log(process.env);
+        console.log(process.env.URL_DB);
+        console.log("DATABASE ENVIROTMENT VARIABLE"+process.env.URL_DB);
         const sum1=sum(1,2);
         assert.equal(sum1,3)
     })
